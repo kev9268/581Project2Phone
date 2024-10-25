5f4da2fb0bd86a21c0c6a426be0b864209896f4e
kev9268 <kevinhoang9268@gmail.com>
2024-10-25 17:16:19 -0600

    Update script.js

--- 581Project2/script.js
+++ 581Project2/script.js
@@ -32,3 +32,95 @@ const filer = document.querySelectorAll(".fileopener");
 filer.forEach((f) => {
   f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
 });
+
+
+
+// -------------------------------------------------------------------------------------
+
+  //motion event
+// DeviceMotionEvent.requestPermission()
+// .then(response => {
+//   if (response == 'granted') {
+//     window.addEventListener('devicemotion', handleMotion, true);
+//   }
+// })
+// .catch(console.error)
+let check_touch = false
+let average_accel = 0;
+
+touchArea.addEventListener('touchend', function(event) {
+  check_touch = false
+});
+
+touchArea.addEventListener('touchstart', function(event) {
+  check_touch = true
+});
+
+function incrementEventCount(event){
+  let counterElement = document.getElementById("num-observed-events")
+  let eventCount = parseInt(counterElement.innerHTML)
+  counterElement.innerHTML = eventCount + 1;
+  average_accel = event.acceleration.x + event.acceleration.y + event.acceleration.z
+
+}
+
+function handleMotion(event) {
+  // updateFieldIfNotNull('Accelerometer_gx', event.accelerationIncludingGravity.x);
+  // updateFieldIfNotNull('Accelerometer_gy', event.accelerationIncludingGravity.y);
+  // updateFieldIfNotNull('Accelerometer_gz', event.accelerationIncludingGravity.z);
+
+  // updateFieldIfNotNull('Accelerometer_x',);
+  // updateFieldIfNotNull('Accelerometer_y', );
+  // updateFieldIfNotNull('Accelerometer_z', event.acceleration.z);
+  if (check_touch) {
+    console.log("X: " +  event.acceleration.x);
+    console.log("Y: " +  event.acceleration.y);
+    console.log("Z: " +  event.acceleration.z);
+    incrementEventCount(event);
+
+    
+  }
+  
+  return 0;
+
+
+
+  // updateFieldIfNotNull('Accelerometer_i', event.interval, 2);
+
+  // updateFieldIfNotNull('Gyroscope_z', event.rotationRate.alpha);
+  // updateFieldIfNotNull('Gyroscope_x', event.rotationRate.beta);
+  // updateFieldIfNotNull('Gyroscope_y', event.rotationRate.gamma);
+  
+}
+
+function reset_input(){
+
+}
+
+function handleOrientation(event) {
+  // updateFieldIfNotNull('Orientation_a', event.alpha);
+  // updateFieldIfNotNull('Orientation_b', event.beta);
+  // updateFieldIfNotNull('Orientation_g', event.gamma);
+  console.log("X: " +  event.alpha);
+  console.log("Y: " +  event.beta);
+  console.log("Z: " +  event.gamma);
+  if (event.alpha == 90){
+    reset_input()
+  }
+  // incrementEventCount();
+}
+
+window.addEventListener('deviceorientation', function(event) {
+  //console.log(event.alpha + ' : ' + event.beta + ' : ' + event.gamma);
+  handleOrientation(event);
+});
+
+window.addEventListener('devicemotion', function(event) {
+  //console.log(event.alpha + ' : ' + event.beta + ' : ' + event.gamma);
+  if (check_touch){
+    handleMotion(event);
+  } else {
+
+  }
+});
+
